4495342f03c9c504c0def08bd7126d0a56e5f80a
sofiabobadillalugo <A01801981@tec.mx>
2025-10-03 07:18:38 -0600

    Create S18_A01801981.ex4.py

---
+++ S18_A01801981.ex4.py
@@ -0,0 +1,7 @@
+numbers = [42, 7, 19, 73, 5, 88, 21, 36]
+max_value = max(numbers)
+print("Maximum value:", max_value)
+min_value = min(numbers)
+print("Minimum value:", min_value)
+sorted_list = sorted(numbers)
+print("Numbers in order:", sorted_list)
